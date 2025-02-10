29fd30243ba1c3a5cb3b21de21aa80c7093e97e5
gedizz <gedizzletsplay@gmail.com>
2025-02-10 11:11:42 -0600

    add set_class method

--- matchmaking/player.py
+++ matchmaking/player.py
@@ -7,10 +7,14 @@ class Player:
         self.proficiency = proficiency
         self.mmr = mmr
         self .igl = igl
+        self.selected_class = "None"
 
     def __repr__(self):
-        return f"Player(id={self.id}, mmr={self.mmr}, proficiency={self.proficiency}, igl={self.igl}"
+        return f"Player(id={self.id}, mmr={self.mmr}, proficiency={self.proficiency}, igl={self.igl} Selected: {self.selected_class}"
 
     @classmethod
     def from_json(cls, json_data: List[Dict]):
         return [cls(player["id"], player["proficiency"], player["mmr"], player["igl"]) for player in json_data]
+
+    def set_class(self, class_name):
+        self.selected_class = class_name
